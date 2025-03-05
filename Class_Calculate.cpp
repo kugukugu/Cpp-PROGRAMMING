4d24be3af968d5328cd299dc21410b5c22fd2d7c
kugukugu <n-a-v-e-r_is_a_firm@naver.com>
2025-03-05 12:13:56 +0900

    Update Class_Calculate.cpp

--- Class_Calculate.cpp
+++ Class_Calculate.cpp
@@ -2,50 +2,104 @@
 
 using namespace std;
 
-class Calculate {
-private:
-    int a;
-    int b;
-    int menu;
-
+class Person {
 public:
-    void input_number() {
-        cout<<"숫자 2개를 입력하세요.." ;
-        cin >> a;
-        cin >> b;
+    char name[10];
+    int age;
+    char* hobby[3];
+
+    Person() {
+        for (int i = 0; i < 3; i++) {
+            hobby[i] = nullptr;
+        }
     }
-
-  void printmenu() {
-        cout << "1.+\n2.-\n3.X\n4.%\n"<<endl;
-        cout << "연산할 메뉴를 고르세요."<<endl;
-        cin >> menu;
-        cout << endl;
-  }
-
-  void print_result() {
-
-        if(menu==1) {
-            cout << a << '+' << b << '=' << a+b << endl;
-        }
-        if(menu==2) {
-            cout << a << '-' << b << '=' << a-b << endl;
-        }
-        if(menu==3) {
-            cout << a << 'X' << b << '=' << a*b << endl;
-        }
-        if(menu==4) {
-            cout << a << '/' << b << '=' << a/b << endl;
-        }
-  }
-
 };
 
-int main() {
-  Calculate c ;
+class PersonManager {
+private:
+    int menuOption;
+    Person p1;
+public:
+    int menu() {
+        do
+        {
+            cout << "메뉴를 선택하시오. " << endl;
+            cout << "1. 회원등록" << endl;
+            cout << "2. 전체 회원 조회" << endl;
+            cout << "3. 전체 회원 수" << endl;
+            cout << "4. 이름 찾기" << endl;
+            cout << "5. 기존 회원 관심사 추가" << endl;
+            cin >> menuOption;
+            if (menuOption < 1 || menuOption >5) {
+                cout << "잘못된 입력" << endl;
+            }
+        } while (menuOption < 1 || menuOption >5);
 
-  c.input_number();
-  c.printmenu();
-  c.print_result();
+        return menuOption;
+    }
 
-  return 0;
-}
+    Person* join_user() {
+        cout << "이름과 나이를 입력하시오. " << endl;
+        cout << "이름: ";
+        cin >> p1.name;
+        cout  << "나이: ";
+        cin >> p1.age;
+        cout << "\n";
+
+        return &p1;
+    }
+
+      void search_user(Person* p[]) {
+          for (int i = 0; p[i] != nullptr; i++) {
+              cout << p[i]->name << endl;
+              cout << p[i]->age << endl;
+              cout << "\n";
+          }
+      }
+
+     /* void total_user(Person* p[]) {
+          for (int i = 0; p[i] != NULL; i++) {
+
+          }
+          cout << "회원 명수: " << i - 1 << endl;
+      }
+
+      void find_name(Person* p[]) {
+          char name[10];
+          cout << "이름을 입력하시오. ";
+          cin >> name;
+
+          for (int i = 0; p[i] != NULL; i++) {
+              if (!= strcmp(&p[i]->name, &name)) {
+                  return p[i];
+              }
+          }
+
+          cout << "찾는 사람이 없습니다" << endl;*/
+};
+
+ int main() {
+     Person* p[100] = { nullptr };
+     PersonManager p1;
+     int menu, i = 0;
+
+       while (i + 1) {
+           menu = p1.menu();
+
+           if (menu == 1) {
+                p[i] = p1.join_user();
+                i++;
+           }
+           else if (menu == 2) {
+                p1.search_user(p);
+           }
+            /*else if (menu == 3) {
+                total_user(p1);
+            }
+            else if (menu == 4) {
+                find_name();
+            }*/
+
+        }
+
+  }

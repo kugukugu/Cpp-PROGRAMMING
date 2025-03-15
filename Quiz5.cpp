85778dfe0bf7a30f1c7e984e94e3363d42a67b51
kugukugu <n-a-v-e-r_is_a_firm@naver.com>
2025-03-15 15:18:30 +0900

    Create Quiz5.cpp

---
+++ Quiz5.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+void swap (int *a, int*b) {
+
+    int temp;
+    temp = *a;
+    *a= *b;
+    *b= temp;
+
+}
+
+void swap (char **a, char **b) {
+
+    char** temp;
+    temp = a;
+    a = b;
+    b= temp;
+
+}
+
+int main() {
+    int a=20;
+    int b=30;
+
+    char *c = "abcd";
+    char *d = "efgh";
+
+    swap(&a, &b);
+    swap(c, d);
+
+    cout << a <<' '<< b << endl;
+    cout << c <<' '<< d << endl;
+}

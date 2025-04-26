f7c274d520789c72fe371e0908cdfe607db37378
kugukugu <n-a-v-e-r_is_a_firm@naver.com>
2025-04-26 14:33:05 +0900

    Update mystring_class.cpp

--- mystring_class.cpp
+++ mystring_class.cpp
@@ -29,6 +29,43 @@ public:
         return length;
     }
 
+    char* getPointer() {
+        return this->ch;
+    }
+
+    mystring& append1 (mystring& str, int pos, int n) {
+        char *ch= new char[this->length+n+1];
+        strcpy(ch, this->ch);
+        char* t = str.getPointer();
+        strncat(ch, t+pos, n);
+        ch[this->length+n]='\0';
+        delete []this->ch ;
+        this->ch = ch;
+        length= strlen(this->ch);
+    }
+
+    mystring& insert1 (int pos, mystring& str) {
+        char *ch=new char[length+str.length1()+1];
+        strncpy(ch, this->ch, pos);
+        strcat(ch, str.getPointer());
+        strcat(ch, this->ch+pos);
+        ch[length+str.length1()]='\0';
+        delete []this->ch ;
+        this->ch = ch;
+        length= strlen(this->ch);
+
+    }
+
+    mystring* replace1(int pos, int n, mystring& str) {
+        char *ch = new char[length-n+str.length1()+1];
+        strncpy(ch, this->ch, pos);
+        strcat(ch, str.getPointer());
+        strcat(ch, this->ch+pos+n);
+        delete []this->ch;
+        this->ch = ch;
+        length= strlen(this->ch);
+    }
+
     void operator+=(const mystring& v1) {
         char *ch = new char [length+strlen(v1.ch)+1];
         strcpy(ch,this->ch);
@@ -57,6 +94,22 @@ public:
         return mys;
     }
 
+    mystring erase1(int pos, int n) {
+        char *test= new char[length-n+1];
+        strncpy(test, this->ch, pos);
+        test[pos]='\0';
+        strcat(test, this->getPointer()+n+pos);
+        delete []this->ch;
+        this->ch = test;
+        length= strlen(this->ch);
+
+        return *this;
+    }
+
+    char at1(int pos) {
+        return *(this->ch+pos);
+    }
+
     size_t find1(char* ch) {
         char *test = strstr(this->ch, ch);
 
@@ -67,6 +120,8 @@ public:
             return -1;
         }
     }
+
+
 
     friend mystring operator+(const mystring& mystring1, const mystring& mystring2);
     friend ostream& operator << (ostream& os, mystring& v);
@@ -122,6 +177,9 @@ int main() {
 
     mystring str1 = "Hello";
     mystring str2 = "World!";
+    mystring str8 = "Good bye";
+    mystring str4 = "Bad";
+    mystring str9 = "Kang Dong Won";
 
     mystring result = str1+str2;
 
@@ -168,6 +226,26 @@ int main() {
     else {
         cout << "못 찾음" << endl;
     }
+
+    str1.append1(str2, 2, 2);
+
+    cout << str1 << endl;
+
+    str2.insert1(4,str1);
+
+    cout << str2 << endl;
+
+    str8.replace1(3, 2, str4);
+
+    cout << str8 << endl;
+
+    str9.erase1(5, 4);
+
+    cout << str9 << endl;
+
+    char kang = str9.at1(3);
+
+    cout << kang << endl;
 
     return 0;
 }
